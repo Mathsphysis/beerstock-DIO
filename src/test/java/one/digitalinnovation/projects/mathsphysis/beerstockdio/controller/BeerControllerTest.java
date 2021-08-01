3f27ea2af11aba9abdc38e3227f95ca84532d87e
Maths Physis <smathsphysis@gmail.com>
2021-08-01 00:56:22 -0400

    test: Added POST endpoint test and utils

--- src/test/java/one/digitalinnovation/projects/mathsphysis/beerstockdio/controller/BeerControllerTest.java
+++ src/test/java/one/digitalinnovation/projects/mathsphysis/beerstockdio/controller/BeerControllerTest.java
@@ -1,11 +1,27 @@
 package one.digitalinnovation.projects.mathsphysis.beerstockdio.controller;
 
+import one.digitalinnovation.projects.mathsphysis.beerstockdio.builder.BeerDTOBuilder;
+import one.digitalinnovation.projects.mathsphysis.beerstockdio.dto.request.BeerDTO;
+import one.digitalinnovation.projects.mathsphysis.beerstockdio.exception.BeerAlreadyRegisteredException;
 import one.digitalinnovation.projects.mathsphysis.beerstockdio.service.BeerService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
+import static one.digitalinnovation.projects.mathsphysis.beerstockdio.utils.JsonConvertionUtils.asJsonString;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
 public class BeerControllerTest {
@@ -23,4 +39,28 @@ public class BeerControllerTest {
 
     @InjectMocks
     private BeerController beerController;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(beerController)
+                .setCustomArgumentResolvers(new PageableHandlerMethodArgumentResolver())
+                .setViewResolvers((s, locale) -> new MappingJackson2JsonView())
+                .build();
+    }
+
+    @Test
+    void whenPOSTIsCalledThenBeerIsCreated() throws BeerAlreadyRegisteredException, Exception {
+        BeerDTO beerDTO = BeerDTOBuilder.builder().build().toBeerDTO();
+
+        when(beerService.createBeer(beerDTO)).thenReturn(beerDTO);
+
+        mockMvc.perform(post(BEER_API_URL_PATH)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(beerDTO)))
+                    .andExpect(status().isCreated())
+                    .andExpect(jsonPath("$.name", is(beerDTO.getName())))
+                    .andExpect(jsonPath("$.brand", is(beerDTO.getBrand())))
+                    .andExpect(jsonPath("$.type", is(beerDTO.getType().toString())));
+    }
+
 }
