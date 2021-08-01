db4c10f46b00a4e481de79b3e651a213da9fd506
Maths Physis <smathsphysis@gmail.com>
2021-08-01 00:08:43 -0400

    test: Initial setup for BeerController tests

---
+++ src/test/java/one/digitalinnovation/projects/mathsphysis/beerstockdio/controller/BeerControllerTest.java
@@ -0,0 +1,26 @@
+package one.digitalinnovation.projects.mathsphysis.beerstockdio.controller;
+
+import one.digitalinnovation.projects.mathsphysis.beerstockdio.service.BeerService;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ExtendWith(MockitoExtension.class)
+public class BeerControllerTest {
+
+    private static final String BEER_API_URL_PATH = "/api/v1/beers";
+    private static final Long VALID_BEER_ID = 1l;
+    private static final Long INVALID_BEER_ID = 2l;
+    private static final String BEER_API_SUBPATH_INCREMENT_URL = "/increment";
+    private static final String BEER_API_SUBPATH_DECREMENT_URL = "/decrement";
+
+    private MockMvc mockMvc;
+
+    @Mock
+    private BeerService beerService;
+
+    @InjectMocks
+    private BeerController beerController;
+}
